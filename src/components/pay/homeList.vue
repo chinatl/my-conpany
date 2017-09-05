e9ca0b4e59385374f72644843518b3af6e76b4d3
chinatl <1663629808@qq.com>
2017-09-05 10:55:35 +0800

    finished all pages

--- src/components/pay/homeList.vue
+++ src/components/pay/homeList.vue
@@ -51,7 +51,7 @@
            </li>
            <li>
                <span>收据总额</span>
-               <span>149.97</span>
+               <span @click='openTab' style="color:blue">149.97</span>
            </li>
            <li>
                <span>理赔总额</span>
@@ -67,10 +67,36 @@
            </li>
            <li>
                <span>拒赔原因</span>
-               <span>拒赔原因</span>
+               <span @click='openBox' style="color:blue">拒赔原因</span>
            </li>
        </ul>
     </div>
+<div class='detailfoud' v-show='boxstatus'>
+    <div class='detailfoud_box'>
+        <div class='title'><span v-html='boxTitle'></span><span class="closeBox fa fa-times" @click='hideBox'></span></div>
+        <div class='wordBox' v-show='wordBox'>
+            <p><span>费用调整原因:</span><span>-</span></p>
+            <p><span>拒付原因:</span><span>-</span></p>
+            <p><span>给付/拒赔/延赔原因:</span><span>-</span></p>
+            <p><span>补充说明:</span><span>所选药物不在报销范围内</span></p>
+        </div>
+        <div class="tabBox" v-show='tabBox'>
+            <p>
+                <span>现金自负金额：149.97</span>
+                <span>额外支付金额：0.00</span>
+
+            </p>
+            <p>
+                <span>当年账户自负金额：0.00</span>
+                <span>统筹支付金额：0.00</span>
+            </p>
+            <p>        
+                <span>历年账户自负金额：0.00</span>
+                <span>自费金额：0.00</span>
+            </p>
+        </div>
+    </div>
+</div>
 </div>
 </template>
 
@@ -87,11 +113,33 @@
                     end_time: '2018-03-17',
                     manager_num: '110000',
                     remark: '1234',
-                }, ]
+                }, ],
+                boxstatus: false,
+                boxTitle: '',
+                wordBox:false,
+                tabBox:false,
             }
         },
         created() {
-          
+
+        },
+        methods: {
+            hideBox() {
+                this.boxstatus = false;
+
+            },
+            openBox() {
+                this.boxstatus = true;
+                this.boxTitle = '拒赔原因';
+                this.wordBox = true;
+                this.tabBox = false;
+            },
+            openTab() {
+                this.boxstatus = true;
+                this.boxTitle = '收据金额明细';
+                this.tabBox = true;
+                this.wordBox = false;
+            }
         }
 
     }
@@ -128,10 +176,35 @@
         text-align: center;
         font-size: 1.2rem;
     }
+    
     .infoContent {
         margin-bottom: 10px;
     }
+    
     a:-webkit-any-link {
         color: #fff;
     }
+    
+    .wordBox p {
+        display: block;
+        margin: 0 auto;
+        padding: 6px 2px;
+    }
+    
+    .wordBox p span:nth-of-type(1) {
+        width: 45%;
+        display: inline-block;
+        text-align: right;
+        margin-right: 5px;
+        font-size: 1.2rem;
+    }
+    
+    .tabBox p {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 12px;
+        font-size: 1.1rem
+    }
+
 </style>
